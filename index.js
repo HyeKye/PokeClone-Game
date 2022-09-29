8dbc1fa3cec5438b524f5ce042ca1bd800db8709
Jordan <jordan.k.wilia@gmail.com>
2022-09-29 15:23:47 -0700

    hooked up the index.js and the html. Have the map rendering correctly with the player in view

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
 const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d') // c = context
 
 canvas.width = 1024
 canvas.height = 576
 
-testing commit
+c.fillStyle = 'white'
+c.fillRect(0, 0, canvas.width, canvas.height)
+
+const image = new Image()
+image.src = './images/pokeStyleMap.png'
+
+image.onload = () => {
+    c.drawImage(image, -1455, -400)
+}
+
+const playerImage = new Image()
+playerImage.src = './images/playerDown.png'
+
+playerImage.onload = () => {
+    c.drawImage(
+        playerImage,
+        0, 
+        0,
+        playerImage.width/4,
+        playerImage.height, 
+        canvas.width/2 - (playerImage.width/4/2), 
+        canvas.height/2 - playerImage.height/2,
+        playerImage.width/4,
+        playerImage.height
+    )
+}
+
