971939b1db2e9e287463f1708bcd9f350c42716f
Jordan <jordan.k.wilia@gmail.com>
2022-09-29 15:36:28 -0700

    wip

--- index.js
+++ index.js
@@ -31,3 +31,67 @@ playerImage.onload = () => {
     )
 }
 
+class Sprite {
+    constructor({ position, velocity, image }) { 
+        this.position = position
+        this.image = image
+    }
+
+    draw() {
+        c.drawImage(this.image, -1455, -400)
+    }
+}
+
+const background = new Sprite({
+    position: {
+    x: -1455,
+    y: -400
+    },
+    image: image
+})
+
+const keys = {
+    w: {
+        pressed: false
+    },
+    a: {
+        pressed: false
+    },
+    s: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    }
+}
+
+function animate() {
+    window.requestAnimationFrame(animate)
+    background.draw
+    c.drawImage(
+        playerImage,
+        0, 
+        0,
+        playerImage.width/4,
+        playerImage.height, 
+        canvas.width/2 - (playerImage.width/4/2), 
+        canvas.height/2 - playerImage.height/2,
+        playerImage.width/4,
+        playerImage.height
+    )
+
+    if ()
+}
+
+window.addEventListener('keydown', (e) => {
+    switch(e.key) {
+        case 'w':
+            break
+        case 'a':
+            break
+        case 's':
+            break
+        case 'd':
+            break
+    }
+})
