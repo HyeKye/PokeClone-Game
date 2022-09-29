71b7f77251e880bb6f1fa6e448963330ae8b61db
Jordan <jordan.k.wilia@gmail.com>
2022-09-29 16:21:31 -0700

    working on collisions

--- index.js
+++ index.js
@@ -4,32 +4,55 @@ const c = canvas.getContext('2d') // c = context
 canvas.width = 1024
 canvas.height = 576
 
+const collisionsMap = [];
+
+for (let i = 0; i < collisions.length; i+= 70){
+    collisionsMap.push(collisions.slice(i, 70 + i))
+}
+
+class Boundary {
+    static width = 48
+    static height = 48
+    constructor ({position}) {
+        this.position = position
+        this.width = 48
+        this.height = 48
+    }
+
+    draw() {
+        c.fillStyle = 'red'
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+}
+
+const boundaries = []
+const offset = {
+    x: -1455,
+    y: -400
+}
+
+collisionsMap.forEach((row, i) => {
+    row.forEach((symbol, j) => {
+        if (symbol === 1025)
+        boundaries.push(
+            new Boundary({
+            position: {
+                x: j * Boundary.width + offset.x,
+                y: i * Boundary.height + offset.y
+        }}))
+    })
+})
+
+console.log(boundaries)
+
 c.fillStyle = 'white'
 c.fillRect(0, 0, canvas.width, canvas.height)
 
 const image = new Image()
 image.src = './images/pokeStyleMap.png'
 
-image.onload = () => {
-    c.drawImage(image, -1455, -400)
-}
-
 const playerImage = new Image()
 playerImage.src = './images/playerDown.png'
-
-playerImage.onload = () => {
-    c.drawImage(
-        playerImage,
-        0, 
-        0,
-        playerImage.width/4,
-        playerImage.height, 
-        canvas.width/2 - (playerImage.width/4/2), 
-        canvas.height/2 - playerImage.height/2,
-        playerImage.width/4,
-        playerImage.height
-    )
-}
 
 class Sprite {
     constructor({ position, velocity, image }) { 
@@ -38,14 +61,14 @@ class Sprite {
     }
 
     draw() {
-        c.drawImage(this.image, -1455, -400)
+        c.drawImage(this.image, this.position.x, this.position.y)
     }
 }
 
 const background = new Sprite({
     position: {
-    x: -1455,
-    y: -400
+    x: offset.x,
+    y: offset.y
     },
     image: image
 })
@@ -67,7 +90,10 @@ const keys = {
 
 function animate() {
     window.requestAnimationFrame(animate)
-    background.draw
+    background.draw()
+    boundaries.forEach(boundary => {
+        boundary.draw()
+    })
     c.drawImage(
         playerImage,
         0, 
@@ -80,18 +106,49 @@ function animate() {
         playerImage.height
     )
 
-    if ()
+    if (keys.w.pressed && lastKey === 'w') background.position.y += 3
+    else if (keys.a.pressed && lastKey === 'a') background.position.x += 3
+    else if (keys.s.pressed && lastKey === 's') background.position.y -= 3
+    else if (keys.d.pressed && lastKey === 'd') background.position.x -= 3
+    
 }
+animate()
 
+let lastKey = ''
 window.addEventListener('keydown', (e) => {
     switch(e.key) {
         case 'w':
+            keys.w.pressed = true
+            lastKey = 'w'
+                break
+        case 'a':
+            keys.a.pressed = true
+            lastKey = 'a'
+                break
+        case 's':
+            keys.s.pressed = true
+            lastKey = 's'
+                break
+        case 'd':
+            keys.d.pressed = true
+            lastKey = 'd'
+                break
+    }
+})
+
+window.addEventListener('keyup', (e) => {
+    switch(e.key) {
+        case 'w':
+            keys.w.pressed = false
             break
         case 'a':
+            keys.a.pressed = false
             break
         case 's':
+            keys.s.pressed = false
             break
         case 'd':
+            keys.d.pressed = false
             break
     }
 })
